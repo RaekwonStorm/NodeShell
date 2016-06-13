20f20e8a9a5ce542ea8fae78078a41d0f090cacb
Chris DeMoll <demoll.chris@gmail.com>
2016-06-13 17:14:19 -0400

    head, tail and cat added

--- bash.js
+++ bash.js
@@ -5,15 +5,34 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  if (data.toString().trim() === "pwd") {
+  var dataArr = data.toString().trim().toLowerCase().split(" ");
+  var prompter = function() {
+    return process.stdout.write('\nprompt > ');
+  }
+
+  if (dataArr[0] === "pwd") {
     commands.pwd();
-  } else if (data.toString().trim() === "date") {
+    prompter();
+  } else if (dataArr[0] === "date") {
     commands.date();
+    prompter();
+  } else if (dataArr[0] === "ls") {
+    commands.ls();
+  } else if (dataArr[0] === "echo") {
+    // console.log(dataArr[0], dataArr.slice(1));
+    commands.echo(dataArr.slice(1).join(" "));
+    prompter();
+  } else if (dataArr[0] === "cat") {
+    commands.cat(dataArr[1]);
+  } else if (dataArr[0] === "head") {
+    commands.head(dataArr[1]);
+  } else if (dataArr[0] === "tail") {
+    commands.tail(dataArr[1]);
   } else {
     var cmd = data.toString().trim(); // remove the newline
     process.stdout.write('You typed: ' + cmd);
   }
 
-  process.stdout.write('\nprompt > ');
+
 
 });
--- commands.js
+++ commands.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 exports.pwd = function () {
   return process.stdout.write(process.argv[1]);
 };
@@ -5,3 +7,51 @@ exports.pwd = function () {
 exports.date = function () {
   return process.stdout.write(new Date().toString());
 };
+
+exports.ls = function() {
+  fs.readdir('.', function(err, files) {
+    if (err) { throw err; }
+    files.forEach(function(file) {
+      process.stdout.write(file.toString() + "\n");
+    });
+    process.stdout.write("prompt > ");
+  });
+}
+
+exports.echo = function(input) {
+  if (input[0] === "$") {
+    var key = input.slice(1).toUpperCase();
+    return process.stdout.write(process.env[key]);
+  }
+  return process.stdout.write(input);
+}
+
+exports.cat = function(file) {
+  if (file[0] === "/") {
+    file = file.slice(1);
+  }
+  fs.readFile(file, function(err, data) {
+    if (err) { throw err; }
+
+    process.stdout.write(data.toString());
+    process.stdout.write("prompt > ");
+  });
+}
+
+exports.head = function(file) {
+  fs.readFile(file, function(err, data) {
+    if (err) { throw err; }
+
+    process.stdout.write(data.toString().split("\n").slice(0, 5).join("\n") + "\n");
+    process.stdout.write("prompt > ");
+  });
+}
+
+exports.tail = function(file) {
+  fs.readFile(file, function(err, data) {
+    if (err) { throw err; }
+
+    process.stdout.write(data.toString().split("\n").slice(-5).join("\n") + "\n");
+    process.stdout.write("prompt > ");
+  });
+}
